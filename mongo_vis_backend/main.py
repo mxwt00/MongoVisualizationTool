71b999e5710e1424aa40a6dff8970c0cfb47dbbd
Max Winter <maxwinter00@gmail.com>
2023-01-09 09:25:14 +0100

    endpoint now working

--- mongo_vis_backend/main.py
+++ mongo_vis_backend/main.py
@@ -14,25 +14,30 @@ class Table:
         self.keys = keys
         self.name = name
 
+    def to_string(self):
+        s = f"{self.name}"
+        for key in self.keys:
+            s += f"  {key}"
+        return s
+
 
 @app.route("/")
-def hello_world():
+def get_tables_and_keys():
     mongodb_client = MongoClient("mongodb+srv://mvt:mvt@testcluster.biadm2g.mongodb.net/?retryWrites=true&w=majority")
     database = mongodb_client["sample_mflix"]
-    collection_names = database.list_collection_names()
-    tables = list()
 
+    collection_names = database.list_collection_names()
+
+    tables = list()
     for name in collection_names:
         collection = database.get_collection(name)
-        doc = collection.find()
-
+        doc = collection.find_one()
         keys = list()
-        for x in doc:
-            print(x.keys)
-            keys.append(x.keys)
-
+        for key in doc:
+            keys.append(key)
         table = Table(name, keys)
-        tables.append(table)
+        tables.append(table.to_string())
 
     mongodb_client.close()
+    print(tables)
     return tables
