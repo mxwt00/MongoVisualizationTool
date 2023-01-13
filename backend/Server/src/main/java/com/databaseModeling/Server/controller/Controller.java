b116c1df64e88ef1ba6d582b6fc25b9e6bba01b3
Max Winter <maxwinter00@gmail.com>
2023-01-13 14:33:22 +0100

    rolled back changes in spring backend

--- backend/Server/src/main/java/com/databaseModeling/Server/controller/Controller.java
+++ backend/Server/src/main/java/com/databaseModeling/Server/controller/Controller.java
@@ -1,12 +1,8 @@
 package com.databaseModeling.Server.controller;
 
 import com.databaseModeling.Server.dto.ConceptionalModelDto;
-import com.databaseModeling.Server.dto.MongoDBConnectionDto;
 import com.databaseModeling.Server.dto.RelationalModelDto;
-import com.databaseModeling.Server.model.MongoDBModel.Table;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 public class Controller {
@@ -22,15 +18,14 @@ public class Controller {
 
     /**
      * Endpoint to convert an entity relationship model to a relational model
+     *
      * @param type The entity relationship model
      * @return The relational model
      */
     @PostMapping("/convert/relational")
     @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:3000", "https://dbmodelling.herokuapp.com"})
     public RelationalModelDto convertToRelational(
-            @RequestBody ConceptionalModelDto type)
-    {
-
+            @RequestBody ConceptionalModelDto type) {
 
 
         ErToRelationalModelTransformer transformer = new ErToRelationalModelTransformer();
@@ -40,43 +35,19 @@ public class Controller {
 
     /**
      * Endpoint to generate sql from a relational model
+     *
      * @param type The relational model
      * @return The generated sql
      */
     @PostMapping("/convert/sql")
     @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:3000", "https://dbmodelling.herokuapp.com"})
     public String convertToSql(
-            @RequestBody RelationalModelDto type)
-    {
-
+            @RequestBody RelationalModelDto type) {
 
 
         RelationalModelToSqlTranslator sqlTranslator = new RelationalModelToSqlTranslator();
         return sqlTranslator.translate(type);
     }
-
-    /**
-     * Endpoint for getting the tables of a MongoDB
-     */
-    @PostMapping("/mongodb/tables")
-    @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:3000", "https://dbmodelling.herokuapp.com"})
-    public String getMongoDbTables(@RequestBody MongoDBConnectionDto mongoDBBConnectionDTO) {
-        /*
-        List<Table> tables = MongoDBUtil.getTables(mongoDBBConnectionDTO);
-        StringBuilder r = new StringBuilder();
-        for (Table table: tables) {
-            for (String key :table.keys) {
-                r.append(key);
-                r.append("\n");
-            }
-            r.append("\n");
-        }
-        return r.toString();
-
-         */
-        return "test working";
-    }
-
 
 }
 
--- backend/Server/src/main/java/com/databaseModeling/Server/model/MongoDBModel/Table.java
+++
@@ -1,9 +0,0 @@
-package com.databaseModeling.Server.model.MongoDBModel;
-
-import java.util.Set;
-
-public class Table {
-    public Set<String> keys;
-    public String name;
-
-}
