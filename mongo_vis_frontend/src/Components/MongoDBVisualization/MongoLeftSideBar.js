5d659ce6d8a5815488a805382a45380155e054fe
Max Winter <maxwinter00@gmail.com>
2023-01-16 14:06:46 +0100

    wip

--- mongo_vis_frontend/src/Components/MongoDBVisualization/MongoLeftSideBar.js
+++ mongo_vis_frontend/src/Components/MongoDBVisualization/MongoLeftSideBar.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
-import {TextField} from "@mui/material";
+import {Button, TextField} from "@mui/material";
 import {ConnectionStates} from "./ConnectionState";
+import axios from "axios";
 
 /**
  * Renders the hole left side bare by orchestrating DragBarManagers for all ErTypes
@@ -8,9 +9,35 @@ import {ConnectionStates} from "./ConnectionState";
  */
 const MongoLeftSideBar = () => {
     const [connectionState, setConnectionState] = useState(ConnectionStates.connectionPending)
+    const [connectionString, setConnectionString] = useState("connection string")
+    const [dbName, setDbName] = useState("database")
+
+    //TODO markierung von tab fixen
 
     function connectToDB() {
+        console.log("connecting to db")
+        //TODO url relativ abfragen
+        const url = "http://127.0.0.1:8081/connect"
 
+        let contentToSend = {
+            connection_string: connectionString,
+            database: dbName
+        };
+
+        axios.post(url, contentToSend).then((response) => {
+            connectionSuccessful(response)
+        }).catch(error => connectionFailed(error))
+    }
+
+    function connectionSuccessful(response) {
+        console.log("data: " + response.data)
+        setConnectionState(ConnectionStates.connected)
+    }
+
+
+    function connectionFailed(error) {
+        console.log(error)
+        setConnectionState(ConnectionStates.connectionFailed)
     }
 
     return (
@@ -28,6 +55,7 @@ const MongoLeftSideBar = () => {
                     label="Connection String"
                     variant="standard"
                     defaultValue="connection string"
+                    onChange={(e) => setConnectionString(e.target.value)}
                 />
                 <div className="spacerSmall"></div>
                 <TextField
@@ -35,7 +63,10 @@ const MongoLeftSideBar = () => {
                     label="DB name"
                     variant="standard"
                     defaultValue="database"
+                    onChange={(e) => setDbName(e.target.value)}
                 />
+                <div className="spacerSmall"></div>
+                <Button variant="outlined" onClick={connectToDB}>Connect</Button>
                 <div className="spacerSmall"></div>
                 <div>{connectionState}</div>
             </div>
